55b44c4e7cdac5e7cd2b5e0839d01ac478601069
5Jaxon <144318757+5Jaxon@users.noreply.github.com>
2024-09-12 11:38:23 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 // 配置 Express 提供静态文件
-app.use('/public',express.static('./public')); // public 目录下放置你的 index.html 和 js 文件
+app.use(express.static(path.join(__dirname, 'public'))); // public 目录下放置你的 index.html 和 js 文件
 
 const width=2560;
 const height=1118;
